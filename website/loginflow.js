f1e8c57b9709a1957da0dd59f5745310562c862c
Saloni <saloni.jain@gida.io>
2023-09-20 14:37:32 +0800

    .

--- website/loginflow.js
+++ website/loginflow.js
@@ -5,8 +5,8 @@ import { Counter } from "k6/metrics";
 import { SharedArray } from 'k6/data';
 
 export let options = {
-  vus: 10000,
-  iterations: 10000,
+  vus: 5000,
+  iterations: 5000,
   thresholds: {
     http_req_receiving: ["p(95)<300"],
   },
